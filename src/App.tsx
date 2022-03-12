7fd2a34b48ca62912844ed025a19e68e17f6349c
Lean Junio <leanjunio@live.com>
2022-03-12 14:58:39 -0500

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.scss";
 import { Card } from "./Card";
 import { Column } from "./Column";
@@ -12,67 +11,21 @@ export type Ticket = {
 export type Direction = "left" | "right";
 
 function App() {
-  const [board, setBoard] = useBoard(["winnie", "brad", "bob", "thomas"]);
+  const { board, moveTicket, addTicketToList } = useBoard([
+    "winnie",
+    "brad",
+    "bob",
+    "thomas",
+  ]);
 
-  // const addTicketToList = (ticket: Ticket, listIndex: number) => {
-  //   const listToAddTo = listUtils[listIndex]["list"];
-  //   const updateList = listUtils[listIndex]["setter"];
+  const openAddTaskPrompt = (listIndex: number) => {
+    const newTask = window.prompt("What to do next?");
 
-  //   updateList([...listToAddTo, ticket]);
-  // };
-
-  // const openAddTaskPrompt = (listIndex: number) => {
-  //   const newTask = window.prompt("What to do next?");
-
-  //   if (newTask) {
-  //     const newTicket = { stage: listIndex, task: newTask };
-  //     addTicketToList(newTicket, listIndex);
-  //   }
-  // };
-
-  // const removeTicketFromCurrentList = (ticket: Ticket) => {
-  //   const listIndex = ticket.stage;
-  //   const listToRemoveFrom = listUtils[listIndex]["list"];
-  //   const setList = listUtils[listIndex]["setter"];
-  //   const filteredList = listToRemoveFrom.filter(
-  //     ({ task }) => task !== ticket.task
-  //   );
-
-  //   setList([...filteredList]);
-  // };
-
-  // const insertTicketToNewList = (ticket: Ticket, placementIndex: number) => {
-  //   const listIndex = ticket.stage;
-  //   const listToAddTo = listUtils[listIndex]["list"];
-  //   const setList = listUtils[listIndex]["setter"];
-  //   const DELETE_COUNT = 0;
-
-  //   listToAddTo.splice(placementIndex, DELETE_COUNT, ticket);
-
-  //   setList([...listToAddTo]);
-  // };
-
-  // const getCurrentTicketPlacement = (ticket: Ticket) => {
-  //   const currentList = listUtils[ticket.stage]["list"];
-  //   const currentTicketPlacement = currentList.findIndex(
-  //     ({ task }) => task === ticket.task
-  //   );
-
-  //   return currentTicketPlacement;
-  // };
-
-  // const moveTicket = (ticket: Ticket, direction: Direction) => {
-  //   const currentTicketPlacement = getCurrentTicketPlacement(ticket);
-  //   removeTicketFromCurrentList(ticket);
-  //   setTicketDestinationStage(ticket, direction);
-  //   insertTicketToNewList(ticket, currentTicketPlacement);
-  // };
-
-  const setTicketDestinationStage = (ticket: Ticket, direction: Direction) => {
-    direction === "left" ? ticket.stage-- : ticket.stage++;
+    if (newTask) {
+      const newTicket = { stage: listIndex, task: newTask };
+      addTicketToList(newTicket, listIndex);
+    }
   };
-
-  console.log(board);
 
   return (
     <div className="App">
@@ -84,8 +37,8 @@ function App() {
             onAdd={openAddTaskPrompt}
             title={list.name}
           >
-            {list.tickets.map((t) => (
-              <Card task={t} onMove={moveTicket} />
+            {list.tickets.map((t, i) => (
+              <Card key={i} task={t} onMove={moveTicket} />
             ))}
           </Column>
         ))}
