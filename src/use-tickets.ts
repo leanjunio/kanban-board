72df2471a24941aeef678e1703834e16f7aa5881
Lean Junio <leanjunio@live.com>
2022-03-12 17:04:19 -0500

    chore: remove unused file

--- src/use-tickets.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export const useTicket = (listNames: string[]) => {
-  const lists = listNames.map((name) => ({ [name]: [] }));
-  const [list, setLists] = useState(lists);
-
-  return [list, setLists];
-};
